96aeafe1da326217dc8115b1020d2683eb1e28df
Faiz <faiz.ggmu@gmail.com>
2018-08-20 18:38:14 +0500

    Router Demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepartmentListComponent } from 'src/app/department-list/department-list.component';
+import { EmployeeListComponent } from 'src/app/employee-list/employee-list.component';
+import { PageNotFoundComponent } from 'src/app/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/department', pathMatch: 'full'},
+  {path: 'department', component: DepartmentListComponent},
+  {path: 'employee', component: EmployeeListComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [DepartmentListComponent, EmployeeListComponent, PageNotFoundComponent];
